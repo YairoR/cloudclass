8e89fe84e81398a2b3dceb7abd587e476320b289
YairoR <yair.ripshtos@gmail.com>
2014-04-16 20:13:50 +0300

    Adding win form

---
+++ CloudClassForms/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace CloudClassForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonPen = new System.Windows.Forms.Button();
+            this.ButtonColors = new System.Windows.Forms.Button();
+            this.ButtonEraser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonPen
+            // 
+            this.ButtonPen.Location = new System.Drawing.Point(798, 45);
+            this.ButtonPen.Name = "ButtonPen";
+            this.ButtonPen.Size = new System.Drawing.Size(75, 23);
+            this.ButtonPen.TabIndex = 1;
+            this.ButtonPen.Text = "button2";
+            this.ButtonPen.UseVisualStyleBackColor = true;
+            this.ButtonPen.Click += new System.EventHandler(this.ButtonPen_Click);
+            // 
+            // ButtonColors
+            // 
+            this.ButtonColors.Location = new System.Drawing.Point(798, 138);
+            this.ButtonColors.Name = "ButtonColors";
+            this.ButtonColors.Size = new System.Drawing.Size(75, 23);
+            this.ButtonColors.TabIndex = 2;
+            this.ButtonColors.Text = "buttonColors";
+            this.ButtonColors.UseVisualStyleBackColor = true;
+            // 
+            // ButtonEraser
+            // 
+            this.ButtonEraser.Location = new System.Drawing.Point(798, 88);
+            this.ButtonEraser.Name = "ButtonEraser";
+            this.ButtonEraser.Size = new System.Drawing.Size(75, 23);
+            this.ButtonEraser.TabIndex = 3;
+            this.ButtonEraser.Text = "button3";
+            this.ButtonEraser.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(885, 648);
+            this.Controls.Add(this.ButtonEraser);
+            this.Controls.Add(this.ButtonColors);
+            this.Controls.Add(this.ButtonPen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonPen;
+        private System.Windows.Forms.Button ButtonColors;
+        private System.Windows.Forms.Button ButtonEraser;
+    }
+}
+
---
+++ CloudClassForms/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloudClassForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPen_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics graphic = CreateGraphics();
+            graphic.FillEllipse(new SolidBrush(Color.FromName("black")), e.X, e.Y, 21, 21);
+        }
+    }
+}
