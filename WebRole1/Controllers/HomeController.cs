c3a5af15d480e2f1649dfe6c0da9a013fe8a10d2
nirb89 <nirb89@gmail.com>
2014-04-16 23:46:36 +0300

    Added web role project

---
+++ WebRole1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebRole1.Models;
+
+namespace WebRole1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Request.IsAuthenticated)
+            {
+                SqlManager sqlManager = new SqlManager("pzze5baa7t.database.windows.net,1433", "hackidc", "hack2014!");
+                ViewBag.courses = sqlManager.fetchUsersCourses(User.Identity.Name);
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
