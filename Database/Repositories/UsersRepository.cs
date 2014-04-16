95c88f6a13f851dbaa67ae98a609d1e4b1a47c3d
YairoR <yair.ripshtos@gmail.com>
2014-04-16 22:35:51 +0300

    Adding repositories

---
+++ Database/Repositories/UsersRepository.cs
@@ -0,0 +1,17 @@
+﻿using Orchestration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Repositories
+{
+    public class UsersRepository : Repository
+    {
+        public IEnumerable<User> GetUsers(string userName)
+        {
+
+        }
+    }
+}
