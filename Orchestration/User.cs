e1926dfaec2c559b32f77d9ad67832317265e702
YairoR <yair.ripshtos@gmail.com>
2014-04-17 03:00:35 +0300

    Repositories created

--- Orchestration/User.cs
+++ Orchestration/User.cs
@@ -8,8 +8,7 @@ namespace Orchestration
 {
     public class User
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string UserName { get; set; }
         public bool IsTeacher { get; set; }
     }
 }
