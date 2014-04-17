d0dd1cb6e89236299d918e366c7c4ca41b32f2d2
YairoR <yair.ripshtos@gmail.com>
2014-04-17 15:34:57 +0300

    Changing forms

--- rectangle/Program.cs
+++ rectangle/Program.cs
@@ -15,7 +15,7 @@ namespace rectangle
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1()); 
+            Application.Run(new WhiteBoard()); 
         }
     }
 }
