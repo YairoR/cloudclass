6a00655591b7f659b09eeff55f63e4487f10ac2c
adibenisrael <adibenisraelrules@gmail.com>
2014-04-17 08:36:46 +0300

    Paint can draw circles as if a line

---
+++ CloudClassForms/Form2.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace CloudClassForms
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.ButtonPen = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // ButtonPen
+            // 
+            this.ButtonPen.Image = ((System.Drawing.Image)(resources.GetObject("ButtonPen.Image")));
+            this.ButtonPen.Location = new System.Drawing.Point(764, 12);
+            this.ButtonPen.Name = "ButtonPen";
+            this.ButtonPen.Size = new System.Drawing.Size(77, 77);
+            this.ButtonPen.TabIndex = 8;
+            this.ButtonPen.UseVisualStyleBackColor = true;
+            this.ButtonPen.Click += new System.EventHandler(this.ButtonPen_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(758, 413);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            this.panel1.Resize += new System.EventHandler(this.panel1_Resize);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 411);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ButtonPen);
+            this.DoubleBuffered = true;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonPen;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ CloudClassForms/Form2.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloudClassForms
+{
+    public partial class Form2 : Form
+    {
+        private Bitmap buffer;
+        private bool penIsClicked = false;
+        private bool penIsDown = false;
+        public Form2()
+        {
+            InitializeComponent();
+
+            panel1_Resize(this, null);
+        }
+
+        private void ButtonPen_Click(object sender, EventArgs e)
+        {
+            if (!penIsClicked)
+            {
+                //unclickEraser();
+                ButtonPen.BackColor = Color.Gray;
+                penIsClicked = true;
+            }
+            else
+            {
+                unclickPen();
+            }
+
+        }
+
+        private void PaintBlueRectangle()
+        {
+            // Draw blue rectangle into the buffer
+            using (Graphics bufferGrph = Graphics.FromImage(buffer))
+            {
+                bufferGrph.DrawRectangle(new Pen(Color.Blue, 1), 1, 1, 100, 100);
+            }
+
+            // Invalidate the panel. This will lead to a call of 'panel1_Paint'
+            panel1.Invalidate();
+        }
+
+        private void unclickPen()
+        {
+            penIsClicked = false;
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // Draw the buffer into the panel
+            e.Graphics.DrawImageUnscaled(buffer, Point.Empty);
+
+        }
+
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            // Resize the buffer, if it is growing
+            if (buffer == null ||
+                buffer.Width < panel1.Width ||
+                buffer.Height < panel1.Height)
+            {
+                Bitmap newBuffer = new Bitmap(panel1.Width, panel1.Height);
+                if (buffer != null)
+                    using (Graphics bufferGrph = Graphics.FromImage(newBuffer))
+                        bufferGrph.DrawImageUnscaled(buffer, Point.Empty);
+                buffer = newBuffer;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            penIsDown = true;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            penIsDown = false;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (penIsDown)
+            {
+                using (Graphics bufferGrph = Graphics.FromImage(buffer))
+                {
+                    bufferGrph.DrawEllipse(new Pen(Color.Blue, 4), e.X, e.Y, 4, 4);
+                }
+                // Invalidate the panel. This will lead to a call of 'panel1_Paint'
+                panel1.Invalidate();
+            }
+        }
+
+
+    }
+}
